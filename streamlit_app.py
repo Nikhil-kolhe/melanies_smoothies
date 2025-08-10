d16c12e17a3f33ab827b530caf7b3c549c15148c
Nikhil kolhe <92266685+Nikhil-kolhe@users.noreply.github.com>
2025-08-10 20:40:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ st.write("The name on your Smoothie will be:", name_on_order)
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
+# st.dataframe(pd_df)
 # st.stop()
 
 ingredient_list = st.multiselect('Choose upto 5 ingredients:', my_dataframe, max_selections=5)
@@ -36,11 +36,11 @@ if ingredient_list:
         ingredients_string += fruit_chosen + ' '
       
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        # st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
       
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+search_on)
       
         # st.text(smoothiefroot_response.json())
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
